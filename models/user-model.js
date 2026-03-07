de2ab854520616556f1cd1324a82ebfe2f405039
Shariar Rafi <shariar.rafi364@gmail.com>
2026-03-07 14:43:28 +0600

    user, product model initially created

--- models/user-model.js
+++ models/user-model.js
@@ -1,2 +1,22 @@
 const mongoose = require("mongoose")
-// mongoose.connect("mongodb://127.0.0.1/27017/")
+
+mongoose.connect("mongodb://127.0.0.1:27017/scatch")
+
+const userSchema = mongoose.Schema({
+    fullname: String,
+    email: String,
+    password: String,
+    profilePic: String,
+    contact: Number,
+    cart:{
+        type: Array,
+        default: []
+    },
+    isAdmin : Boolean,
+    orders:{
+        type: Array,
+        default: []
+    },
+})
+
+module.exports = mongoose.model("user", userSchema)
